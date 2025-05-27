3c911643b7ab6285f3f6d2ebf9f2ee821979689d
nhantacb <nhantacbvn@gmail.com>
2025-05-27 22:51:01 +0700

    Create File Installer

--- WinFormsApp/WinFormsApp/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
+++ WinFormsApp/WinFormsApp/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bad494642140487ec854cfdf8ee42bc4e61835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3182b2e8c1f87d27314421668f16f2def160edd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
